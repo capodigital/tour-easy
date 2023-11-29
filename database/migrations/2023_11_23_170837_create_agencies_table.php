01b663ba781b9fbb7574d7ce8f7e7e65bff24cc4
capodigital <capodigitalapp@gmail.com>
2023-11-29 14:08:35 -0500

    migraciones

---
+++ database/migrations/2023_11_23_170837_create_agencies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agencies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('tradename', 100)->unique()->nullable();
+            $table->string('taxname', 100)->unique()->nullable();
+            $table->string('taxcode', 30)->unique()->nullable();
+            $table->string('owner', 50)->nullable();
+            $table->string('address', 250)->nullable();
+            $table->string('email', 250)->unique()->nullable();
+            $table->text('web')->nullable();
+            $table->text('notes')->nullable();
+            $table->string('phone', 30)->nullable();
+            $table->foreignId('city_id')->references('id')->on('cities')->onDelete('set null');
+   
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agencies');
+    }
+};
--- database/migrations/2023_11_29_181825_create_cities_table.php
+++ database/migrations/2023_11_29_181825_create_cities_table.php
@@ -11,9 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('agencia', function (Blueprint $table) {
+        Schema::create('cities', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');         
+            $table->foreignId('country_id')->references('id')->on('countries')->nullable()->onDelete('set null');
+
         });
     }
 
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('agencia');
+        Schema::dropIfExists('cities');
     }
 };
