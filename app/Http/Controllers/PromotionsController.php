616cca94e4f393d4866022e8ba111a0239072345
capodigital <capodigitalapp@gmail.com>
2023-12-17 11:08:48 -0500

    crud promotions

--- app/Http/Controllers/PromotionsController.php
+++ app/Http/Controllers/PromotionsController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\PromotionsResource;
+use App\Models\Documents;
 use App\Models\Promotions;
+use App\Models\Socialmedias;
 use Illuminate\Http\Request;
 
 class PromotionsController extends Controller
@@ -30,7 +32,42 @@ class PromotionsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'platform' => 'required',
+            
+
+        ]);
+
+        $data = $request->only(['platform', 'notes', 'itinerary_id', 'tour_id']);
+
+       
+
+        //Almacenar los datos en la base de datos
+        $promotion = Promotions::create($data);
+
+        foreach ($request->socialmedias as $socialmedia) {
+            Socialmedias::create([
+                'url' => $socialmedia->url,
+                'description' => $socialmedia->description,
+                'typeredes_id' => $socialmedia->typeredes_id,
+                'socialmediaable_id' => $promotion->id,
+                'socialmediaable_type' => 'App\Models\Promotions'
+            ]);
+        }
+        foreach ($request->documents as $document) {
+            Documents::create([
+                'url' => $document->url,
+                'name' => $document->name,
+                'document_path' => $document->document_path,
+                'size' => $document->size,
+                'ext' => $document->ext,
+                'documentable_id' => $promotion->id,
+                'documentable_type' => 'App\Models\Promotions'
+            ]);
+        }
+
+        $promotion->refresh();
+        return new PromotionsResource($promotion);
     }
 
     /**
@@ -52,16 +89,54 @@ class PromotionsController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Promotions $promotions)
+    public function update(Request $request, Promotions $promotion)
     {
-        //
+        $request->validate([
+            'platform' => 'required',
+            
+        ]);
+
+        $data = $request->only(['platform', 'notes', 'itinerary_id', 'tour_id']);
+
+      
+       
+        //Almacenar los datos en la base de datos
+        $promotion->update($data);
+        
+        Socialmedias::where('socialmediaable_id', $promotion->id)->delete();
+        foreach ($request->socialmedias as $socialmedia) {
+            Socialmedias::create([
+                'url' => $socialmedia->url,
+                'description' => $socialmedia->description,
+                'typeredes_id' => $socialmedia->typeredes_id,
+                'socialmediaable_id' => $promotion->id,
+                'socialmediaable_type' => 'App\Models\Promotions'
+            ]);
+        }
+        Documents::where('documentable_id', $promotion->id)->delete();
+        foreach ($request->documents as $document) {
+            Documents::create([
+                'url' => $document->url,
+                'name' => $document->name,
+                'document_path' => $document->document_path,
+                'size' => $document->size,
+                'ext' => $document->ext,
+                'documentable_id' => $promotion->id,
+                'documentable_type' => 'App\Models\Promotions'
+            ]);
+        }
+
+        $promotion->refresh();
+        return new PromotionsResource($promotion);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Promotions $promotions)
+    public function destroy(Promotions $promotion)
     {
-        //
+        $promotion->delete();
+
+        return response()->json($promotion);
     }
 }
