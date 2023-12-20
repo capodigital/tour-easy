02ea3add3737c51bbe827238a6b7db8d0c6eeebf
ArielLeyva <msomnium.info@gmail.com>
2023-12-20 14:07:01 -0500

    Corregido error al guardar itinerario

--- database/migrations/2023_12_07_002514_create_itineraries_table.php
+++ database/migrations/2023_12_07_002514_create_itineraries_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -27,8 +26,8 @@ return new class extends Migration
             $table->boolean('outoflour')->default(false);
             $table->string('carrier', 150)->nullable();
             $table->string('carrier_id', 150)->nullable();
-            $table->time('showcheck');
-            $table->time('showtime');
+            $table->time('showcheck')->nullable();
+            $table->time('showtime')->nullable();
             $table->boolean('notify')->default(false);
             $table->timestamps();
         });
