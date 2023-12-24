ba5045ec85e86682c00472296a2e5a498936cd13
capodigital <capodigitalapp@gmail.com>
2023-12-24 14:13:08 -0500

    date

--- database/migrations/2023_12_07_002514_create_itineraries_table.php
+++ database/migrations/2023_12_07_002514_create_itineraries_table.php
@@ -20,8 +20,8 @@ return new class extends Migration {
             $table->foreignId('contact_id')->nullable()->references('id')->on('contacts')->onDelete('set null');
             $table->foreignId('supplier_id')->nullable()->references('id')->on('suppliers')->onDelete('set null');
             $table->string('name', 150)->nullable();
-            $table->dateTime('startdate')->nullable();
-            $table->dateTime('enddate')->nullable();
+            $table->date('startdate')->nullable();
+            $table->date('enddate')->nullable();
             $table->text('notes')->nullable();
             $table->boolean('outoflour')->default(false);
             $table->string('carrier', 150)->nullable();
