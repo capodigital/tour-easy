796f3dd997dbed64861994586844dbda499d0688
ArielLeyva <msomnium.info@gmail.com>
2024-02-01 21:24:00 -0500

    Adición del modelo TourContact

---
+++ app/Models/TourContact.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TourContact extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'tour_id',
+        'person_id'
+    ];
+
+    public $table = 'tourcontacts';
+}
