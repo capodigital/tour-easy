b1dc35031604be375c8298718bce496807cdecac
capodigital <capodigitalapp@gmail.com>
2024-02-01 20:47:17 -0500

    main

--- app/Http/Controllers/ToursController.php
+++ app/Http/Controllers/ToursController.php
@@ -346,7 +346,7 @@ class ToursController extends Controller
 
         $tour = Tours::find($request->tour_id);
 
-        $tour->persons()->syncWithoutDetaching([$request->contacts]);
+        $tour->persons()->syncWithoutDetaching($request->contacts);
 
         $tour->refresh();
         return new ToursResource($tour);
